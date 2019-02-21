68a211e0c5e66933eaf0557ef29304a00cd0bae1
Alvie Rahman <alvierahman90@gmail.com>
2019-02-21 16:10:23 +0000

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "-h" ]
+then
+	echo "USAGE: build.sh 'path/to/resource/pack/src' 'output'"
+	exit
+fi
+
+current_directory="$(pwd)"
+cd "$1"
+zip -r -0 "$2" .
+mv "$2" "$current_directory"
+
